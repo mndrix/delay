df75549596529ff48c16bd0faf8429ff51909d44
Michael Hendricks <michael@ndrix.org>
2014-12-24 10:28:18 -0700

    Release v0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# v0.3.0 (2014-12-24)
+
+  * Support is_dict/1
+  * Support is_dict/2
+  * Support dict_pairs/3
+
 # v0.2.1
 
   * Support string_codes/2
--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(delay).
 title('Avoid instantiation errors for built-in predicates').
 
-version('0.2.1').
-download('http://packs.ndrix.com/delay/delay-0.2.1.tgz').
+version('0.3.0').
+download('https://github.com/mndrix/ddata/archive/v0.3.0.zip').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
-home('http://packs.ndrix.com/delay/index.html').
+home('https://github.com/mndrix/delay').
